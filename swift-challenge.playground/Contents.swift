7d052fc760f727729766bce2d9b1a0251a622b2c
Cameron Bernhardt <cambernhardt@me.com>
2020-05-17 01:31:59 -0400

    Draw rudimentary borders (out of order)

---
+++ swift-challenge.playground/Contents.swift
@@ -0,0 +1,85 @@
+//: A MapKit based Playground
+
+import MapKit
+import AppKit
+import PlaygroundSupport
+
+//class StateOverlay: NSObject, MKOverlay {
+//    var name: String
+//    var coordinate: CLLocationCoordinate2D
+//    var boundingMapRect: MKMapRect
+//    
+//    init(name: String) {
+//        self.name = name
+//        
+//        
+//    }
+//}
+
+struct Coord: Codable {
+    var lat: Float
+    var lng: Float
+    
+    func toCL() -> CLLocationCoordinate2D {
+        let clLat = CLLocationDegrees(exactly: lat) ?? CLLocationDegrees()
+        let clLng = CLLocationDegrees(exactly: lng) ?? CLLocationDegrees()
+        
+        return CLLocationCoordinate2D(latitude: clLat, longitude: clLng)
+    }
+}
+
+struct Border: Codable {
+    var state: String
+    var coords: [Coord]
+}
+
+class MapDelegate: NSObject, MKMapViewDelegate {
+    func mapView(_ mapView: MKMapView,
+                 rendererFor overlay: MKOverlay) -> MKOverlayRenderer {
+        if let poly = overlay as? MKPolygon {
+            let renderer = MKPolygonRenderer(overlay: poly)
+            renderer.lineWidth = 3
+            renderer.strokeColor = .red
+            renderer.fillColor = NSColor.red.withAlphaComponent(0.3)
+            
+            return renderer
+        } else {
+            return MKOverlayRenderer(overlay: overlay)
+        }
+    }
+}
+
+let US_CENTER = CLLocationCoordinate2D(latitude: 39.8283, longitude: -98.5795)
+let US_SPAN = MKCoordinateSpan(latitudeDelta: 57, longitudeDelta: 30)
+let MAP_SIZE = 800
+
+// Set up map
+let mapView = MKMapView(frame: CGRect(x: 0, y: 0,
+                                      width: MAP_SIZE, height: MAP_SIZE))
+let delegate = MapDelegate()
+mapView.delegate = delegate
+
+var mapRegion = MKCoordinateRegion()
+mapRegion.center = US_CENTER
+mapRegion.span = US_SPAN
+mapView.setRegion(mapRegion, animated: true)
+
+// Read border data
+var borders: [Border] = []
+if let filePath = Bundle.main.path(forResource: "borders", ofType: "json"),
+    let borderData = try? Data(contentsOf: URL(fileURLWithPath: filePath)) {
+    
+    let decoder = JSONDecoder()
+    if let readBorders = try? decoder.decode([Border].self, from: borderData) {
+        borders = readBorders
+    }
+}
+
+for border in borders {
+    let coords = border.coords.map { $0.toCL() }
+    let poly = MKPolygon(coordinates: coords, count: coords.count)
+    mapView.addOverlay(poly)
+}
+
+// Add the created mapView to our Playground Live View
+PlaygroundPage.current.liveView = mapView
