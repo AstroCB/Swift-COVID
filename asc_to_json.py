b3cc4b0e73e452a5ded6d517881c80ca50ab027c
Cameron Bernhardt <cambernhardt@me.com>
2020-05-17 01:31:20 -0400

    Create conversion parser

---
+++ asc_to_json.py
@@ -0,0 +1,40 @@
+# Convert bordpnts.asc file to JSON data
+import json
+
+IN_FILE = "bordpnts.asc"
+OUT_FILE = "borders.json"
+STATE_IND = 1
+LAT_IND = 3
+LNG_IND = 4
+
+read_format = False
+borders = {}
+with open(IN_FILE) as border_data:
+    for line in border_data:
+        if not read_format:
+            read_format = True
+        else:
+            # Extract data from csv-ish-formatted line
+            data = list(map(lambda s: s.strip(), line.split(",")))
+            state1, state2 = data[STATE_IND].split("-")
+            lat = float(data[LAT_IND])
+            # Longitude doesn't include negative in data set for some reason
+            lng = -float(data[LNG_IND])
+
+            # Insert lat/long border point for both states
+            for state in [state1, state2]:
+                existing = borders.get(state, [])
+                existing.append({"lat": lat, "lng": lng})
+                borders[state] = existing
+
+# Convert JSON representation to something that will translate better to Swift
+borders_out = []
+for state, coords in borders.items():
+    borders_out.append({
+        "state": state,
+        "coords": coords
+    })
+
+with open(OUT_FILE, "w") as output_file:
+    json.dump(borders_out, output_file)
+
